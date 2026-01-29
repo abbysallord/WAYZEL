f2884821db441483a5e8841ae558eec90705d4bf
abbysallord <dshenoyh@gmail.com>
2026-01-29 23:19:09 +0530

    bug fixes

--- Frontend/index.js
+++ Frontend/index.js
@@ -125,7 +125,7 @@ voiceButtons.forEach(btn => {
 
 // Generate Audio guide button Logic
 
-const GENERATE_AUDIO_GUIDE_API_URL = "https://wayzel-api.onrender.com";
+const GENERATE_AUDIO_GUIDE_API_URL = "https://wayzel-api.onrender.com/generate-audio-guide";
 
 generateButton.addEventListener('click', async () => {
   generateButton.disabled = true;
